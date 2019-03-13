921dc3cd3d52fd7a906fd1213565d21490c353b2
Marci Clark <marcig612@gmail.com>
2019-03-13 20:44:48 +0000

    added start method to cli

--- lib/nyt_cli.rb
+++ lib/nyt_cli.rb
@@ -2,11 +2,32 @@ class NewYorkTimesBestsellers::CLI
   
   def call 
     NewYorkTimesBestsellers::Scraper.new.make_bestsellers 
-    puts "New York Times bestsellers"
+    puts "New York Times bestsellers top 5"
     start 
   end 
   
   def start
+    puts ""
+    puts "Which genre would you like to see?  Fiction, nonfiction, or children's?"
+    input = gets.strip.to_i
+    
+    print_bestsellers(input)
+    
+    puts ""
+    puts "Would you like to see another genre?  Enter Y or N"
+    
+    input = gets.strip.downcase
+    if input == "y"
+      start 
+    elseif input == "n"
+      puts ""
+      puts "Thanks for stopping by!"
+      exit 
+    else 
+      puts ""
+      puts "I don't recognize your answer."
+      start 
+    end 
   end 
   
 end 
