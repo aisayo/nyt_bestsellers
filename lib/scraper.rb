223e614b24ed4d8b56bfe17fc4ba030e207ea0e6
Marci Clark <marcig612@gmail.com>
2019-03-13 14:53:11 +0000

    added methods to scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,3 +7,10 @@ class NewYorkTimesBestsellers::Scraper
   def scrape_bestsellers_index
     self.get_page.css()
   end 
+  
+  def add_bestsellers
+    scrape_bestsellers_index.each do |b|
+      NewYorkTimesBestsellers::Bestsellers.new_from_index_page(b)
+    end 
+  end 
+end 
