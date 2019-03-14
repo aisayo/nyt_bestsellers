39b8b0b0b6a1747a2b71241f7f209372573ffaf4
Marci Clark <marcig612@gmail.com>
2019-03-14 12:12:48 +0000

    modified scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
 
-class Scraper
+class NewYorkTimesBestsellers::Scraper
+  
+  #   attr_accessor :title, :author, :genre, :description, :url
+  
+  # @@all = []
+  
+  # def initialize(title=nil, author=nil, url=nil)
+  #   @title = title 
+  #   @author = author 
+  #   @genre = genre 
+  #   @description = description
+  #   @url = url 
+  #   @@all << self 
+  # end 
+  
+  # def self.all 
+  #   @@all
+  # end 
   
   def page
     Nokogiri::HTML(open("https://www.nytimes.com/books/best-sellers/"))
