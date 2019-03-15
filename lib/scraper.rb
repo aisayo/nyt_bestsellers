22c36a92e99ede3d9f621f9e687ff739cc20e3c7
Marci Clark <marcig612@gmail.com>
2019-03-15 13:13:12 +0000

    added self.scrape method to scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,6 +30,12 @@ class NewYorkTimesBestsellers::Scraper
     @week 
   end 
   
+  def self.scrape 
+    @doc = Nokogiri::HTML (open("https://www.nytimes.com/books/best-sellers/"))
+    @scraping_fiction = @doc.css("")
+    @scraping_nonfiction = @doc.css("")
+    @scraping_childrens = @doc.css("")
+  
   def scrape_details
     # date = @doc.search("<div class="date-range tooltip" data-tooltip="Lists are published early online"> == $0   </div>")
     # <div class="date-range tooltip" data-tooltip="Lists are published early online">March 24, 2019    </div>
