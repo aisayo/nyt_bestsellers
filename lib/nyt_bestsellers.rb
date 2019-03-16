f51b6594df7c5c66a769e570314b45bdc27e71ce
Marci Clark <marcig612@gmail.com>
2019-03-16 17:32:00 +0000

    added requirements to nyt_bestsellers folder

--- lib/nyt_bestsellers.rb
+++ lib/nyt_bestsellers.rb
@@ -1,4 +1,13 @@
-module NewYorkTimesBestsellers
-end
+require 'bundler'
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
-require_relative '../config/environment'
+Bundler.require 
+
+require_all './lib'
+
+require_relative '../lib/scraper'
+require_relative '../lib/bestsellers'
+require_relative '../lib/nyt_cli'
+require_relative '../lib/nyt_bestsellers/version'
