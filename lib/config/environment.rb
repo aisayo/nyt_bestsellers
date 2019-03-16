d3be4eb0736baac7f62e852184ddff134354fcb2
Marci Clark <marcig612@gmail.com>
2019-03-16 16:42:12 +0000

    modified environment folder

--- lib/config/environment.rb
+++ lib/config/environment.rb
@@ -6,3 +6,8 @@ require 'open-uri'
 Bundler.require 
 
 require_all './lib'
+
+require_relative '../lib/scraper'
+require_relative '../lib/bestsellers'
+require_relative '../lib/nyt_cli'
+require_relative '../lib/nyt_bestsellers/version'
