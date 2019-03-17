1196073616685f3a84c3c2b7836f2b5aade2212a
Marci Clark <marcig612@gmail.com>
2019-03-17 17:15:48 +0000

    added scrape to fiction method and modified other methods

--- lib/bestsellers.rb
+++ lib/bestsellers.rb
@@ -41,34 +41,34 @@ class NewYorkTimesBestsellers::Bestsellers
   def self.scrape_fiction
     html = open("https://www.barnesandnoble.com/b/the-new-york-times-bestsellers-hardcover-fiction/_/N-1p3r")
     doc = Nokogiri::HTML(html)
-    title = doc.css("").text.gsub(/\t/, "")
+    category = doc.css("div.html content-node.html-embed-container").text
+    title = doc.css("<div.product-shelf-title pr-m").text.gsub(/\t/, "")
     author = doc.css("div.product-shelf-author contributors")
     description = doc.css("")
-    category = doc.css("div.html content-node.html-embed-container").text
     
-    fiction = self.new(title, author, category)
+    fiction = self.new(category, title, author)
   end 
   
   def self.scrape_nonfiction
     html = open("https://www.barnesandnoble.com/b/the-new-york-times-bestsellers-hardcover-nonfiction/_/N-1p5q")
     doc = Nokogiri::HTML(html)
+    category = doc.css("").text
     title = doc.css("").text.gsub(/\t/, "")
     author = doc.css("")
     description = doc.css("")
-    category = doc.css("").text
     
-    nonfiction = self.new(title, author, category)
+    nonfiction = self.new(category, title, author)
   end 
   
   def self.scrape_childrens
     html = open("https://www.barnesandnoble.com/b/the-new-york-times-bestsellers-childrens-picture-books/_/N-1p3p")
     doc = Nokogiri::HTML(html)
+    category = doc.css("").text
     title = doc.css("").text.gsub(/\t/, "")
     author = doc.css("")
     description = doc.css("")
-    category = doc.css(").text
     
-    childrens = self.new(title, author, category)
+    childrens = self.new(category, title, author)
   end 
   
 end 
