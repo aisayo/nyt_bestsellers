cddcca77ebfe1d4eacb661f2a54c89d418f82ead
Marci Clark <marcig612@gmail.com>
2019-03-17 04:49:06 +0000

    added list_bestsellers method to cli

--- lib/nyt_cli.rb
+++ lib/nyt_cli.rb
@@ -15,27 +15,27 @@ class NYT_bestsellers::CLI
   
   def list_categories
     puts "Which genre would you like to see?  Fiction, nonfiction, or children's?"
+    puts "Please select another category by typing 'category' or type 'exit' to leave"
     NYT_bestsellers::bestsellers.all.each.with_index(1) do |bestseller, i|
       puts "#{i}. #{bestseller.category}"
-      puts "Would you like to see another genre?  Enter Y or N"
-    end 
-  end 
-    
-    input = gets.strip.downcase
-    if input == "y"
-      start 
-    elseif input == "n"
-      puts ""
-      puts "Happy reading!"
-      exit 
-    else 
-      puts ""
-      puts "I don't recognize your answer."
-      start 
     end 
   end 
   
   def list_bestsellers
+    input = nil 
+    while input != "exit"
+    input = gets.strip.downcase 
+    if input == "category"
+      list_categories 
+    elsif input.to_i.between?(0, NYT_bestsellers::bestsellers.all.size)
+    selected_category = NYT_bestsellers::bestsellers.find_rbestsellers(input)
+    puts "#{selected_category.title}"
+    else
+      puts "I do not recognize your answer. "
   end 
+  
+  def goodbye
+    puts "Happy reading!"
+  end
   
 end 
