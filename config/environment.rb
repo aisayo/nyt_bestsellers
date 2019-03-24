4ce7bfa596e790b8dabe51a6ec3922225cb98011
Marci Clark <marcig612@gmail.com>
2019-03-24 01:16:25 +0000

    fixed requirement issues

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,8 @@ require 'open-uri'
 
 # Bundler.require 
 
-require_all './lib'
+# require_all './lib'
 
 require_relative '../lib/nyt_bestsellers/bestsellers'
-require_relative '../lib/nyt_bestsellers/nyt_bestsellers'
 require_relative '../lib/nyt_bestsellers/nyt_cli'
 require_relative '../lib/nyt_bestsellers/version'
